e3122ac9bdb9eb9592ef818454c401018513e25c
Rishabh Dev <rdev748@gmail.com>
2025-03-11 16:33:55 +0530

    all good

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ const { initializeSocket } = require('./socket');
 const server=http.createServer(app);
 initializeSocket(server);
 
-const port=process.env.port || 3000;
+const port=process.env.PORT || 3000;
 server.listen(port,()=>{
     console.log(`server listening on port: ${port}`);
 });
